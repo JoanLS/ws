b78c8e8d20ea5ccd5cf6979623a0c7c85aa75d77
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2017-05-01 12:41:56 +0200

    Add back form handling and add practical info view

--- django_wedding/django_wedding/urls.py
+++ django_wedding/django_wedding/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from main.views import Homepage, RSVPLogin, RSVP, FAQ
+from main.views import Homepage, RSVPLogin, RSVP, FAQ, Practical
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', Homepage.as_view(), name="home"),
     url(r'^faq/$', FAQ.as_view(), name="faq"),
+    url(r'^info/$', Practical.as_view(), name="practical"),
     url(r'^rsvp/$', RSVPLogin.as_view(), name="rsvp_login"),
     url(r'^rsvp/(?P<code>[a-zA-Z]{3}[0-9]{3})/$', RSVP.as_view(), name="rsvp")
 ]
--- django_wedding/main/views.py
+++ django_wedding/main/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 from main.forms import RSVPLoginForm, GuestRSVPForm, GuestCommentsForm, GuestChildrenForm
@@ -14,6 +14,9 @@ class Homepage(TemplateView):
 
 class FAQ(TemplateView):
     template_name = "faq.html"
+
+class Practical(TemplateView):
+    template_name = "practical.html"
 
 class RSVPLogin(FormView):
     template_name = 'rsvp_login.html'
@@ -42,13 +45,37 @@ class RSVP(TemplateView):
         guests = []
         for guest in code.guests.all():
             guests.append(guest)
-            guest_forms.append(GuestRSVPForm(instance=guest))
+            guest_forms.append(GuestRSVPForm(instance=guest, prefix="guest"+str(guest.id))) 
 
         context = self.get_context_data(**kwargs)
         context['guests'] = guests
         context['guest_forms'] = guest_forms
-        context['comments_form'] = GuestCommentsForm(instance=code.comments.first())
-        context['children_form'] = GuestChildrenForm(instance=code.children.first())
+        context['comments_form'] = GuestCommentsForm(instance=code.comments.first(), prefix="comments") 
+        context['children_form'] = GuestChildrenForm(instance=code.children.first(), prefix="children") 
 
 
         return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs): 
+        code = GuestCode.objects.get(code=kwargs["code"]) 
+ 
+        guest_forms = [] 
+        guests = [] 
+        for guest in code.guests.all(): 
+            guest_form = GuestRSVPForm(request.POST, instance=guest, prefix="guest"+str(guest.id)) 
+            if guest_form.is_valid(): 
+                guest_form.save() 
+ 
+        comments_form = GuestCommentsForm(request.POST, instance=code.comments.first(), prefix="comments") 
+        children_form = GuestChildrenForm(request.POST, instance=code.children.first(), prefix="children") 
+        if children_form.is_valid(): 
+            guest_children = children_form.save(commit=False) 
+            guest_children.code = code 
+            guest_children.save() 
+ 
+        if comments_form.is_valid(): 
+            guest_comments = comments_form.save(commit=False) 
+            guest_comments.code = code 
+            guest_comments.save()             
+ 
+        return redirect(reverse('rsvp', kwargs={'code': kwargs["code"].lower()}))
