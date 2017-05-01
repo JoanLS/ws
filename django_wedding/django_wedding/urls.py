ce7846ef9505a4f2418780aa8090c8821c8c2fed
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2017-05-01 17:19:52 +0200

    Translations setup

--- django_wedding/django_wedding/urls.py
+++ django_wedding/django_wedding/urls.py
@@ -13,15 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from main.views import Homepage, RSVPLogin, RSVP, FAQ, Practical
 
 urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+]
+
+urlpatterns += i18n_patterns(
     url(r'^admin/', admin.site.urls),
     url(r'^$', Homepage.as_view(), name="home"),
     url(r'^faq/$', FAQ.as_view(), name="faq"),
     url(r'^info/$', Practical.as_view(), name="practical"),
     url(r'^rsvp/$', RSVPLogin.as_view(), name="rsvp_login"),
     url(r'^rsvp/(?P<code>[a-zA-Z]{3}[0-9]{3})/$', RSVP.as_view(), name="rsvp")
-]
+)
+
+from django.conf import settings
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        url(r'^rosetta/', include('rosetta.urls')),
+    ]
