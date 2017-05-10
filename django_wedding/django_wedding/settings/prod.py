48d8a4f58a5ec7aa6df599d9611e533c9069b8dc
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2017-05-10 23:27:51 +0200

    Static path was wrong

--- django_wedding/django_wedding/settings/prod.py
+++ django_wedding/django_wedding/settings/prod.py
@@ -131,4 +131,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "../public/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "../../public/static")
