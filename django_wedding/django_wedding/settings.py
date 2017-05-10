bd0bee14486ae0f9d46da70f4960b1cca3187b66
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2017-05-10 22:36:26 +0200

    Update settings

--- django_wedding/django_wedding/settings.py
+++ django_wedding/django_wedding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2p32(9qr+t0ah$sfp0myuo4-@s1x%a4sj6xb-8#w@a8s3ztkes'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ws.gsmproductions.com']
 
 
 # Application definition
