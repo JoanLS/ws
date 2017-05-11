89d07a8c5261ba1d925af36122c80896cde83bf8
Bastien Gorissen <bastien.gorissen@engie.com>
2017-05-11 16:00:59 +0200

    Work on the admin and the dropshadow in jumbotron

--- django_wedding/main/admin.py
+++ django_wedding/main/admin.py
@@ -3,12 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from main.models import Guest, GuestCode, GuestChildren, GuestComments
+
 # Register your models here.
 
 
 class GuestAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'code', 'coming_private_ceremony', 'coming_public_ceremony')
-    list_filter = ('coming_private_ceremony', )
+    list_display = ('first_name', 'last_name', 'code',
+                    'coming_private_ceremony', 'coming_public_ceremony',
+                    'coming_reception', 'coming_dinner', 'coming_party')
+    list_filter = ('coming_private_ceremony', 'coming_public_ceremony',
+                   'coming_reception', 'coming_dinner', 'coming_party')
 
 
 class GuestAdminInline(admin.TabularInline):
@@ -27,11 +31,13 @@ class GuestCommentsInlineAdmin(admin.StackedInline):
 
 
 class GuestCodeAdmin(admin.ModelAdmin):
-    inlines = (GuestAdminInline, GuestChildrenAdminInline, GuestCommentsInlineAdmin)
+    inlines = (GuestAdminInline, GuestChildrenAdminInline,
+               GuestCommentsInlineAdmin)
 
 
 class GuestCommentsAdmin(admin.ModelAdmin):
-    list_display = ('code', 'dietary_restrictions', 'music_request', 'comments')
+    list_display = ('code', 'dietary_restrictions', 'music_request',
+                    'comments')
 
 
 admin.site.register(Guest, GuestAdmin)
