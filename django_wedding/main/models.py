b9229759f020c658e820e91da53d9e1c860fd597
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2017-05-11 23:51:54 +0200

    Added field when people reply

--- django_wedding/main/models.py
+++ django_wedding/main/models.py
@@ -7,6 +7,7 @@ from django.db import models
 # Create your models here.
 class GuestCode(models.Model):
     code = models.CharField(max_length=6)
+    responded = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.code
